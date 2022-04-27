a75f2c530a76089e6ee86dbfffb0b528641e1280
Alex <alekscherniaev@gmail.com>
2022-04-27 11:08:10 +0300

    create seed user and task

---
+++ server/db/seeders/20220427065925-demo-user.js
@@ -0,0 +1,18 @@
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const hashPassword = await bcrypt.hash('Admin123', 10);
+    await queryInterface.bulkInsert('Users', [{
+      name: 'Admin',
+      email: 'admin@admin.ru',
+      password: hashPassword,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {});
+  },
+};
