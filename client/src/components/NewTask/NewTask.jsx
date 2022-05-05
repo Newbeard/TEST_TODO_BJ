525e0a1deae7becba16db9f02faca3f3f10f17a0
Alex <alekscherniaev@gmail.com>
2022-05-05 15:16:11 +0300

    create modal

--- client/src/components/NewTask/NewTask.jsx
+++ client/src/components/NewTask/NewTask.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import {newTaskFromServer } from '../../redux/actions/task.action'
 import { useDispatch} from 'react-redux'
+import  Modal  from '../Modal/Modal'
+import { useState } from 'react'
 
 function NewTask(props) {
   const dispatch = useDispatch()
+  const [isModal, setModal] = useState(false)
+  const onClouseModal = () =>{
+    setModal(false)
+  }
+
   const handlerSubmit = (event) => {
   event.preventDefault();
   const form = event.target;
   const payload = Object.fromEntries(new FormData(form));
   dispatch(newTaskFromServer(payload));
+  setModal(true)
 }
 
+
   return (
-    
+    <>
     	<form className="uk-flex" onSubmit={handlerSubmit}>
 		
 				<div className="uk-inline">
@@ -29,6 +38,8 @@ function NewTask(props) {
 			
 				<button className="uk-button uk-button-secondary btnNewTask">Add</button>
 		  </form>
+     {isModal && < Modal onClouseModal={onClouseModal}/>}
+      </>  
   );
 }
 
